0ea74d5cc350f7caa2ee3dfbab3695ac6ab0c447
HYUNGU, KANG <gusrn1423@naver.com>
2019-12-13 19:00:09 +0900

    Update index.js

--- web/src/index.js
+++ web/src/index.js
@@ -5,14 +5,7 @@ import App from './pages/App';
 import registerServiceWorker from './services/registerServiceWorker';
 import * as firebase from 'firebase';
 
-const config = {
-    apiKey: "AIzaSyDCiZ3bcSplClhvDBtQleH0gAgY9fOg97c",
-    authDomain: "catch-7e353.firebaseapp.com",
-    databaseURL: "https://catch-7e353.firebaseio.com",
-    projectId: "catch-7e353",
-    storageBucket: "catch-7e353.appspot.com",
-    messagingSenderId: "216812482275"
-};
+const config = {};
 firebase.initializeApp(config);
 registerServiceWorker();
 
